05d88068508dbfcc59327ffaa4f689454d5239c3
Tanishkakalra <87311124+Tanishkakalra@users.noreply.github.com>
2021-08-31 15:27:32 +0530

    Add files via upload

---
+++ 03_program_arithemetic operators.cpp
@@ -0,0 +1,27 @@
+/* 
+Name - Tanishka Kalra
+ Enrollment No. - 0827CI201187
+ Branch and Section - CSIT 3
+ 
+ 
+ 
+ 
+ program -3 “Write a Program for Perform All Arithmetic Operations
+(‘+’,’-“,’*’,’/’,’ %’)”
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	 int a , b;
+	 cout<<"enter the first number";
+	 cin>>a;
+	 cout<<"enter the second number";
+	 cin>>b;
+	 cout<<"the sum of two number is"<<a+b;
+	 cout<<"the substraction of two number is"<<a-b;
+	 cout<<"the multiplication of two number is"<<a*b;
+	 cout<<"the division of two number is"<<a/b;
+	 
+	 return 0;
+}
