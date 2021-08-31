b58001f01ca2e1810d9cbc1bb1f889e055ceb96c
Tanishkakalra <87311124+Tanishkakalra@users.noreply.github.com>
2021-08-31 15:26:58 +0530

    Add files via upload

---
+++ 02_program_area of circle.cpp
@@ -0,0 +1,24 @@
+/* 
+Name - Tanishka Kalra
+ Enrollment No. - 0827CI201187
+ Branch and Section - CSIT 3
+ 
+ 
+ 
+ 
+ program -2 “Write a Program for Calculate Area and Perimeter of the Circle”
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r, a , c ;
+	cout<<"enter the radius of circle";
+	cin>>r;
+	c=2*3.14*r;
+	a=3.14*r*r;
+	cout<<"the area of circle is"<<a;
+	cout<<" the circumference of circle is"<<c;
+	return 0;
+}
