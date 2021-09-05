6d7c8ba1235dde3183bf9aef63d1a3affc33a209
Tanishkakalra <87311124+Tanishkakalra@users.noreply.github.com>
2021-09-05 19:20:59 +0530

    Add files via upload

---
+++ 11_program_even odd.cpp
@@ -0,0 +1,27 @@
+/* 
+Name - Tanishka Kalra
+ Enrollment No. - 0827CI201187
+ Branch and Section - CSIT 3
+ 
+ 
+ 
+ 
+ program 11:    “Write a Program to input a Number and Check whether it is even or odd”
+ */
+ 
+#include <iostream>
+using namespace std ;
+int main ()
+{
+	int a ;
+	cout <<"Enter the number"<<endl;
+	cin>>a;
+	
+	if ( a%2==0)
+	{
+		cout<<"The given number is an even number";	
+	}
+	else 
+	cout<<"The given is an odd number";
+	return 0;
+}
