b9623936294ad813b18392227993e2cb4fab5098
Tanishkakalra <87311124+Tanishkakalra@users.noreply.github.com>
2021-09-05 18:30:09 +0530

    Add files via upload

---
+++ 09_program_comparing numbers.cpp
@@ -0,0 +1,28 @@
+/* 
+Name - Tanishka Kalra
+ Enrollment No. - 0827CI201187
+ Branch and Section - CSIT 3
+ 
+ 
+ 
+ 
+ program -9 “Program for input two number and check whether it is Equal or not”
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{  
+
+    int a, b;
+    cout<<"Enter two number to compare : ";
+    cin>>a>>b;
+    if(a==b)
+    {
+        cout<<"Given numbers are equal  ";
+    }
+    else 
+    cout<<"Given numbers not are equal  ";
+
+    return 0 ;
+}
